7f4616afe9b31b85debbaebb92852328cdebe4d7
unknown <21114290013@ACADEMICO.IESB.BR>
2022-09-12 22:20:03 -0300

    oi

---
+++ app/Controllers/Http/CursosController.ts
@@ -0,0 +1,9 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CursosController {
+    store(){
+        const dados= {nome:'Joao',duracao:2, cursoId:'p'}
+        return dados
+    }
+    
+}
